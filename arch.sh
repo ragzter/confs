5880c43d833cf30fa460639a98d042249deed037
Ragnar Hedenskog <ragnar.hedenskog@gmail.com>
2018-09-27 19:44:57 +0200

    Add Firefox to pacman script

--- arch.sh
+++ arch.sh
@@ -1,4 +1,4 @@
 
 # Very incomplete
 
-pacman -S --noconfirm xorg virtualbox-guest-utils ratpoison sudo xterm emacs chromium git mlocate ghc ghc-static ripgrep
+pacman -S --noconfirm xorg virtualbox-guest-utils ratpoison sudo xterm emacs chromium git mlocate ghc ghc-static ripgrep firefox
