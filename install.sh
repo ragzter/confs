14d93774f015021c1fcd2d067d0d7040b3fafa5f
Ragnar Hedenskog <ragnar.hedenskog@gmail.com>
2019-10-04 22:30:31 +0200

    Install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s `pwd`/.emacs ~/.emacs
+ln -s `pwd`/.xinitrc ~/.xinitrc
+ln -s `pwd`/.Xmodmap ~/.Xmodmap
+ln -s `pwd`/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/.Xresources ~/.Xresources
+
